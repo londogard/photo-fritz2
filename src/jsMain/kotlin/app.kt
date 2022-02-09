c12404ffcb620d4005d124c8fd3424a9af741eba
Lundez <hampus.londogard@gmail.com>
2022-02-09 19:36:26 +0100

    feature: Cleaned up code and fixed so that app can't crash.

--- src/jsMain/kotlin/app.kt
+++ src/jsMain/kotlin/app.kt
@@ -3,6 +3,7 @@ import dev.fritz2.binding.RootStore
 import dev.fritz2.components.clickButton
 import dev.fritz2.components.data.File
 import dev.fritz2.components.file
+import dev.fritz2.components.showAlertToast
 import dev.fritz2.dom.html.Canvas
 import dev.fritz2.dom.html.Img
 import dev.fritz2.dom.html.render
@@ -26,6 +27,7 @@ object OnnxInferenceStore : RootStore<String>("") {
     val tracking = tracker()
 
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     val processImage = handle<Pair<Canvas, Img>> { b64, (targetCanvas, srcImg) ->
         tracking.track {
             kotlin.runCatching {
@@ -39,25 +41,21 @@ object OnnxInferenceStore : RootStore<String>("") {
                 val input = TensorHelper.tensorToInput(tensor)
 
                 val ir = ONNX.loadInferenceSession()
-                // println("Loaded")
                 val out = ir.run(input).await()
-
                 val outTensor = out["output"] as Tensor
-                // js("outTensor *= 255.0")
                 val outData = outTensor.data as Float32Array
-                // println("Float32")
 
                 for (i in 0 until outData.length) {
                     outData[i] = if (outData[i] > 1) 255f else outData[i] * 255f
                 }
-                // println("Minified float32...")
+
                 val intOut = TensorHelper.float32ToUInt8Clamped(outData)
-                // println("Created int")
+
                 val imgData = ImageData(intOut, width, height)
                 imgContext.putImageData(imgData, 0.0, 0.0)
-                // println("Put img")
+
                 targetCanvas.domNode.toDataURL()
-            }.onFailure { println("Failed: $it") }.getOrThrow()// .also { println("Donezo!") }
+            }.onFailure { showAlertToast { alert { title("Failed Inference") } } }.getOrDefault(b64)
 
         }
     }
